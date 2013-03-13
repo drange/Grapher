7ff32bc76c10dd71a974fc3dfcca845c6bfd1e5a
Pål Grønås Drange <github@drange.net>
2013-03-13 16:45:32 +0100

    Added connected feedback vertex set algorithm

--- src/no/uib/ii/algo/st8/algorithms/FeedbackVertexSet.java
+++ src/no/uib/ii/algo/st8/algorithms/FeedbackVertexSet.java
@@ -45,4 +45,50 @@ public class FeedbackVertexSet {
 		}
 		return currentBestFvs;
 	}
+
+
+	public static <V, E> Collection<V> findExactConnectedFeedbackVertexSet(
+			SimpleGraph<V, E> graph) {
+		if (graph == null)
+			throw new NullPointerException("Input graph to FVS was null!");
+
+		if (isAcyclic(graph))
+			return new HashSet<V>();
+
+		PowersetIterator<V> subsets = new PowersetIterator<V>(graph.vertexSet());
+
+		Collection<V> vertices = new HashSet<V>(graph.vertexSet().size());
+		Collection<V> fvs = new HashSet<V>(graph.vertexSet().size());
+
+		Collection<V> currentBestFvs = graph.vertexSet();
+
+		while (subsets.hasNext()) {
+			fvs = subsets.next();
+
+			if (fvs.size() >= currentBestFvs.size())
+				continue;
+
+			vertices.clear();
+			vertices.addAll(graph.vertexSet());
+			vertices.removeAll(fvs);
+			SimpleGraph<V, E> hfvs = InducedSubgraph.inducedSubgraphOf(graph,
+					vertices);
+			if (!new ConnectivityInspector<V, E>(hfvs).isGraphConnected()) continue;
+
+			vertices.clear();
+			vertices.addAll(graph.vertexSet());
+			vertices.removeAll(fvs);
+
+			SimpleGraph<V, E> h = InducedSubgraph.inducedSubgraphOf(graph,
+					vertices);
+
+			if (isAcyclic(h))
+				currentBestFvs = fvs;
+		}
+		return currentBestFvs;
+	}
+
+
+
+
 }
