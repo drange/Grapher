14ceb6f417938eb3b249b03cbce553778152db20
Håvard Haug <xar003@student.uib.no>
2015-04-07 15:03:46 +0200

    Added clone and dot-graph generation output for TestGraph

--- src/no/uib/ii/algo/st8/tests/TestGraph.java
+++ src/no/uib/ii/algo/st8/tests/TestGraph.java
@@ -1,5 +1,7 @@
 package no.uib.ii.algo.st8.tests;
 
+import no.uib.ii.algo.st8.util.Neighbors;
+
 import org.jgrapht.EdgeFactory;
 import org.jgrapht.graph.SimpleGraph;
 
@@ -16,5 +18,30 @@ public class TestGraph extends SimpleGraph<Integer, Integer> {
 				return (int) Math.pow(2, arg0) * (int) Math.pow(3, arg1);
 			};
 		});
+	}
+	
+	@Override
+	public TestGraph clone(){
+		TestGraph g = new TestGraph();
+		for(Integer k : this.vertexSet()){
+			g.addVertex(k);
+		}
+		for(Integer i : this.vertexSet()){
+			for(Integer j : this.edgesOf(i)){
+				g.addEdge(i, Neighbors.opposite(this, i, j));
+			}
+		}
+		return g;
+	}
+	
+	public void DotGraph(){
+		System.out.println("graph myGraph{");
+		for(Integer i : this.vertexSet()){
+			for(Integer j : this.edgesOf(i))
+				if(Neighbors.opposite(this, i, j) > i)
+					System.out.println(i + "--" + Neighbors.opposite(this, i, j) + ";");
+		}
+		System.out.println("}");
+		
 	}
 }
